92e0f10f815c1cd7629d2a4b3025acb8c2796177
Shovel Maman <shovelmn12@gmail.com>
2025-07-25 09:45:50 +0200

    Updated theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { App } from "@/app";
 import { EventsBusProvider } from "@/events";
 import { LoggerSync } from "@/logger";
-import { Grommet, caludeTheme, StrictMode, createRoot } from "@/theme";
+import { Grommet, hpe, StrictMode, createRoot } from "@/theme";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Grommet theme={caludeTheme} themeMode="auto" full>
+    <Grommet theme={hpe} themeMode="auto" full>
       <EventsBusProvider>
         <LoggerSync>
           <App />
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -4,5 +4,4 @@ export { Switch as WSwitch, Route, useLocation } from "wouter";
 export { StrictMode } from "react";
 export type { JSX } from "react";
 export { createRoot } from "react-dom/client";
-// export { hpe as theme } from "grommet-theme-hpe";
 export * from "./data";
