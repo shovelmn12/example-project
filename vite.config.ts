a563079e3f83ed2c1a12c4f0bf55c1bfa130349f
Shovel Maman <shovelmn12@gmail.com>
2025-08-04 19:25:32 +0200

    WIP

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,14 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react({ plugins: [["@swc/plugin-styled-components", {}]] })],
+  plugins: [
+    react({
+      plugins: [
+        ["babel-plugin-react-compiler", {}],
+        ["@swc/plugin-styled-components", {}],
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
