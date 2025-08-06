967cc8f34641e22a05df6f5a1501d83434599471
Shovel Maman <shovelmn12@gmail.com>
2025-08-06 20:48:12 +0200

    Remove/Un-needed Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import { App } from "@/app";
-import { EventsBusProvider } from "@/events";
 import { LoggerSync } from "@/logger";
 import { StrictMode } from "@/theme";
 import { render } from "@/utils";
@@ -7,10 +6,8 @@ import { render } from "@/utils";
 render(
   document.getElementById("root")!,
   <StrictMode>
-    <EventsBusProvider>
-      <LoggerSync>
-        <App />
-      </LoggerSync>
-    </EventsBusProvider>
+    <LoggerSync>
+      <App />
+    </LoggerSync>
   </StrictMode>
 );
