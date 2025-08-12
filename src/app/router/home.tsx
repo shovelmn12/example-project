c8d6d120f05d49e67b7e21dd2b1e356a0c7b564d
Shovel Maman <shovelmn12@gmail.com>
2025-08-12 08:26:58 +0200

    Init

--- src/app/router/home.tsx
+++ src/app/router/home.tsx
@@ -1,6 +1,7 @@
 import { useStrings } from "@/localizations";
 import { Button, type JSX, Page, PageContent } from "@/theme";
-import { useCallback, useLocation } from "@/utils";
+import { none, type Option, some, useCallback, useLocation } from "@/utils";
+import { useEffect, useState } from "react";
 
 export function HomeScreen(): JSX.Element {
   const [, navigate] = useLocation();
@@ -19,7 +20,41 @@ export function HomeScreen(): JSX.Element {
           onClick={goToProjects}
           label={strings.home.actions.go_to_projects}
         />
+        <WebTransportButton />
       </PageContent>
     </Page>
   );
+}
+
+function WebTransportButton(): JSX.Element {
+  const [wt, setWt] = useState<Option<WebTransport>>(none);
+  const onClick = useCallback(() => {
+    try {
+      setWt(some(new WebTransport("https://[::1]:4433")));
+    } catch (e) {
+      console.error(e);
+      setWt(none);
+    }
+  }, [setWt]);
+
+  useEffect(() => {
+    if (wt._tag === "None") {
+      return;
+    }
+
+    const client = wt.value;
+
+    client.ready
+      .then(() => console.log("READY"))
+      .catch((e) => {
+        console.error(e);
+        setWt(none);
+      });
+
+    return () => client.close();
+  }, [wt, setWt]);
+
+  console.log(wt);
+
+  return <Button label="Connect" onClick={onClick} />;
 }
